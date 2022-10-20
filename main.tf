21466e15016c1c8c6c971d403cf0046599c6b753
José Zavaleta <zepolsoft@gmail.com>
2022-10-20 12:19:33 -0300

    update tf files

--- main.tf
+++ main.tf
@@ -9,25 +9,25 @@ terraform {
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = "dop_v1_e0d5fd37b65cdeaeca715f34028bf6bca784a23a4706ef5d30fc04eded5f812d"
+  token = var.do_token
 }
 
 # Create a new Web Droplet in the nyc2 region
 resource "digitalocean_droplet" "jenkins" {
   image    = "ubuntu-22-04-x64"
   name     = "jenkins-vm"
-  region   = "nyc1"
+  region   = var.region
   size     = "s-2vcpu-2gb"
-  ssh_keys = [data.digitalocean_ssh_key.jornada.id]
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
 }
 
-data "digitalocean_ssh_key" "jornada" {
-  name = "Jornada"
+data "digitalocean_ssh_key" "ssh_key" {
+  name = var.ssh_key_name
 }
 
 resource "digitalocean_kubernetes_cluster" "k8s" {
   name    = "k8s"
-  region  = "nyc1"
+  region  = var.region
   version = "1.24.4-do.0"
 
   node_pool {
@@ -35,4 +35,19 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
     size       = "s-2vcpu-2gb"
     node_count = 2
   }
+}
+
+variable "region" { default = "" }
+
+variable "do_token" { default = "" }
+
+variable "ssh_key_name" { default = "" }
+
+output "jenkins_ip" {
+    value = digitalocean_droplet.jenkins.ipv4_address
+}
+
+resource "local_file" "foo" {
+    content = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+    filename = "kube_config.yaml"
 }
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+region       = "nyc1"
+ssh_key_name = "Jornada"
+do_token     = "dop_v1_2c8dd7a3b3e79537215729cc0f17328bed8a31bfd9fa13bc4e42205433d7d724"
